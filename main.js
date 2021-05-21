063a5e0d161f66c7a8acd13fec1afc5f6e687335
wasifraza909 <wasifraza909@gmail.com>
2021-05-21 23:45:13 +0500

    Project Completed

---
+++ main.js
@@ -0,0 +1,35 @@
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarTogglerIcon = document.querySelector('.toggler-icon');
+const body = document.querySelector('.body');
+const navbar = document.querySelector('.navbar');
+const navbarNav = document.querySelector('.navbar-nav');
+const navbarDev = document.querySelector('.navbar-div');
+const navLinks = document.querySelectorAll('.nav-link');
+
+let navToggle = false;
+
+navbarToggler.addEventListener('click', function () {
+  if (navToggle === false) {
+    navbarTogglerIcon.classList.remove('fa-bars');
+    navbarTogglerIcon.classList.add('fa-times');
+    navbarTogglerIcon.style.paddingBottom = '1.5rem';
+    navbarTogglerIcon.style.paddingLeft = '5rem';
+    navLinks.style.textAlign = 'center';
+
+    navToggle = true;
+  } else {
+    navbarTogglerIcon.classList.remove('fa-times');
+    navbarTogglerIcon.classList.add('fa-bars');
+    navToggle = false;
+    body.style.opacity = 1;
+  }
+});
+
+document.addEventListener('click', (e) => {
+  const activeNavLink = document.querySelector('.active');
+
+  if (e.target.classList.contains('nav-link')) {
+    activeNavLink.classList.remove('active');
+    e.target.classList.add('active');
+  }
+});
