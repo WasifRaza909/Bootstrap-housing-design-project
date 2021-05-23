6243452b1e9201005d1761f593aaa033736c41aa
wasifraza909 <wasifraza909@gmail.com>
2021-05-23 07:54:19 +0500

    Made it  responsive for veriety of devices

--- main.js
+++ main.js
@@ -9,21 +9,48 @@ const navLinks = document.querySelectorAll('.nav-link');
 let navToggle = false;
 
 navbarToggler.addEventListener('click', function () {
-  if (navToggle === false) {
+  if (!navToggle) {
+    navToggle = true;
+
     navbarTogglerIcon.classList.remove('fa-bars');
     navbarTogglerIcon.classList.add('fa-times');
-    navbarTogglerIcon.style.paddingBottom = '1.5rem';
-    navbarTogglerIcon.style.paddingLeft = '5rem';
-    navLinks.style.textAlign = 'center';
+    navbarTogglerIcon.style.paddingBottom = '0rem';
+    navbarTogglerIcon.style.marginLeft = '5rem';
+    navbarTogglerIcon.style.marginBottom = '0.2rem';
+    // navLinks.style.textAlign = 'center';
 
-    navToggle = true;
+    body.style.opacity = 1;
   } else {
+    navToggle = false;
     navbarTogglerIcon.classList.remove('fa-times');
     navbarTogglerIcon.classList.add('fa-bars');
-    navToggle = false;
+    navbarTogglerIcon.style.marginLeft = '5rem';
+    navbarTogglerIcon.style.marginBottom = '0rem';
+
     body.style.opacity = 1;
   }
 });
+
+// if (!navToggle) {
+// }
+// if (navToggle) {
+//   console.log('WORLD');
+// }
+
+// navbarToggler.addEventListener('click', function () {
+//   navToggle = true;
+//   // navbarTogglerIcon.classList.remove('fa-bars');
+//   // navbarTogglerIcon.classList.add('fa-times');
+//   // navbarTogglerIcon.style.paddingBottom = '1.5rem';
+//   // navbarTogglerIcon.style.paddingLeft = '5rem';
+//   // navLinks.style.textAlign = 'center';
+
+//   // navToggle = true;
+// });
+
+// if (!navToggle) {
+//   navbarToggler.addEventListener('click', function () {});
+// }
 
 document.addEventListener('click', (e) => {
   const activeNavLink = document.querySelector('.active');
